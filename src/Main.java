b56171f082bd52eaeb36b185cdcc2ad0bd37a2b2
Udit Bhati <55918713+udit19281@users.noreply.github.com>
2020-11-20 18:10:23 +0530

    init
    uploaded files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Application.launch(MainMenu.class,args);
+    }
+}
---
+++ src/MainMenu.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+    public class MainMenu extends Application {
+        @Override
+        public void start(Stage stage) throws Exception {
+            stage.setTitle("Hello World");
+            stage.show();
+        }
+
+}
