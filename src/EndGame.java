a87643611ca25ff7c9cb630ad9277d641794739d
vidhi448 <55681509+vidhi448@users.noreply.github.com>
2020-11-20 19:24:28 +0530

    try

--- src/EndGame.java
+++ src/EndGame.java
@@ -1,2 +1,12 @@
-public class EndGame {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class EndGame extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World");
+        stage.show();
+    }
+
 }
