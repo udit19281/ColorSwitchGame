eb684bb1060c9b0061c62990d67722f95e003288
vidhi448 <55681509+vidhi448@users.noreply.github.com>
2020-11-22 15:46:21 +0530

    obstacle

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,6 +1,7 @@
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.scene.image.ImageView;
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,6 +12,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -85,7 +87,7 @@ public class MainGUI extends Application {
                 "-fx-text-fill:#ffffff;");
         btn1.setOnAction(e->{
             System.out.println("Pressed button 1 in main menu");
-            Group root2 = CircleObstacle();
+            Group root2 = SquareObstacle();
             GamePlay(stage,scene,root2);
         });
 
@@ -421,15 +423,23 @@ public class MainGUI extends Application {
         Circle circ4=new Circle(150.0f,250.0f,135.0f);
 
         Shape clip1= Shape.subtract(arc1,circ1);
-        clip1.setFill(Color.BLUE);
+        clip1.setFill(Color.AQUA);
         Shape clip2= Shape.subtract(arc2,circ2);
-        clip2.setFill(Color.RED);
+        clip2.setFill(Color.BLUE);
         Shape clip3= Shape.subtract(arc3,circ3);
         clip3.setFill(Color.YELLOW);
         Shape clip4= Shape.subtract(arc4,circ4);
-        clip4.setFill(Color.DARKVIOLET);
+        clip4.setFill(Color.DEEPPINK);
 
-        Group root=new Group(clip1,clip2,clip3,clip4);
+        Image image = new Image("star.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(80);
+        imageView.setFitWidth(80);
+        imageView.setX(110);
+        imageView.setY(200);
+        imageView.setPreserveRatio(true);
+
+        Group root=new Group(clip1,clip2,clip3,clip4,imageView);
 
 //        Scene scene=new Scene(root,600,600);
         RotateTransition rotateTransition = new RotateTransition();
@@ -442,6 +452,236 @@ public class MainGUI extends Application {
 
         return root;
     }
+
+    public Group LineObstacle() {
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle2 = new Rectangle();
+        Rectangle rectangle3 = new Rectangle();
+        Rectangle rectangle4 = new Rectangle();
+        Rectangle rectangle5 = new Rectangle();
+        Rectangle rectangle6 = new Rectangle();
+        Rectangle rectangle7 = new Rectangle();
+        Rectangle rectangle8 = new Rectangle();
+
+        rectangle.setX(15.0f);
+        rectangle.setY(75.0f);
+        rectangle.setWidth(15.0f);
+        rectangle.setHeight(120.0f);
+        rectangle.setFill(Color.AQUA);
+
+        rectangle2.setX(65.0f);
+        rectangle2.setY(75.0f);
+        rectangle2.setWidth(15.0f);
+        rectangle2.setHeight(120.0f);
+        rectangle2.setFill(Color.YELLOW);
+
+        rectangle3.setX(115.0f);
+        rectangle3.setY(75.0f);
+        rectangle3.setWidth(15.0f);
+        rectangle3.setHeight(120.0f);
+        rectangle3.setFill(Color.BLUE);
+
+        rectangle4.setX(165.0f);
+        rectangle4.setY(75.0f);
+        rectangle4.setWidth(15.0f);
+        rectangle4.setHeight(120.0f);
+        rectangle4.setFill(Color.DEEPPINK);
+
+        rectangle5.setX(215.0f);
+        rectangle5.setY(75.0f);
+        rectangle5.setWidth(15.0f);
+        rectangle5.setHeight(120.0f);
+        rectangle5.setFill(Color.AQUA);
+
+        rectangle6.setX(265.0f);
+        rectangle6.setY(75.0f);
+        rectangle6.setWidth(15.0f);
+        rectangle6.setHeight(120.0f);
+        rectangle6.setFill(Color.YELLOW);
+
+        rectangle7.setX(315.0f);
+        rectangle7.setY(75.0f);
+        rectangle7.setWidth(15.0f);
+        rectangle7.setHeight(120.0f);
+        rectangle7.setFill(Color.BLUE);
+
+        rectangle8.setX(365.0f);
+        rectangle8.setY(75.0f);
+        rectangle8.setWidth(15.0f);
+        rectangle8.setHeight(120.0f);
+        rectangle8.setFill(Color.DEEPPINK);
+
+
+        TranslateTransition translate = new TranslateTransition();
+        translate.setByX(400);
+        translate.setToX(200);
+        translate.setDuration(Duration.millis(3000));
+        translate.setCycleCount(500);
+        translate.setAutoReverse(true);
+        translate.setNode(rectangle);
+        translate.play();
+
+        TranslateTransition translate2 = new TranslateTransition();
+        translate2.setByX(400);
+        translate2.setToX(200);
+        translate2.setDuration(Duration.millis(3000));
+        translate2.setCycleCount(500);
+        translate2.setAutoReverse(true);
+        translate2.setNode(rectangle2);
+        translate2.play();
+
+        TranslateTransition translate3 = new TranslateTransition();
+        translate3.setByX(400);
+        translate3.setToX(200);
+        translate3.setDuration(Duration.millis(3000));
+        translate3.setCycleCount(500);
+        translate3.setAutoReverse(true);
+        translate3.setNode(rectangle3);
+        translate3.play();
+
+        TranslateTransition translate4 = new TranslateTransition();
+        translate4.setByX(400);
+        translate4.setToX(200);
+        translate4.setDuration(Duration.millis(3000));
+        translate4.setCycleCount(500);
+        translate4.setAutoReverse(true);
+        translate4.setNode(rectangle4);
+        translate4.play();
+
+        TranslateTransition translate5 = new TranslateTransition();
+        translate5.setByX(400);
+        translate5.setToX(-200);
+        translate5.setDuration(Duration.millis(3000));
+        translate5.setCycleCount(500);
+        translate5.setAutoReverse(true);
+        translate5.setNode(rectangle5);
+        translate5.play();
+
+        TranslateTransition translate6 = new TranslateTransition();
+        translate6.setByX(400);
+        translate6.setToX(-200);
+        translate6.setDuration(Duration.millis(3000));
+        translate6.setCycleCount(500);
+        translate6.setAutoReverse(true);
+        translate6.setNode(rectangle6);
+        translate6.play();
+
+        TranslateTransition translate7 = new TranslateTransition();
+        translate7.setByX(400);
+        translate7.setToX(-200);
+        translate7.setDuration(Duration.millis(3000));
+        translate7.setCycleCount(500);
+        translate7.setAutoReverse(true);
+        translate7.setNode(rectangle7);
+        translate7.play();
+
+        TranslateTransition translate8 = new TranslateTransition();
+        translate8.setByX(400);
+        translate8.setToX(-200);
+        translate8.setDuration(Duration.millis(3000));
+        translate8.setCycleCount(500);
+        translate8.setAutoReverse(true);
+        translate8.setNode(rectangle8);
+        translate8.play();
+
+        Group root = new Group(rectangle,rectangle2,rectangle3,rectangle4,rectangle5,rectangle6,rectangle7,rectangle8);
+        return root;
+
+    }
+
+    public Group SquareObstacle() {
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle2 = new Rectangle();
+        Rectangle rectangle3 = new Rectangle();
+        Rectangle rectangle4 = new Rectangle();
+
+        rectangle.setX(150.0f);
+        rectangle.setY(200.0f);
+        rectangle.setWidth(15.0f);
+        rectangle.setHeight(200.0f);
+        rectangle.setFill(Color.AQUA);
+
+        rectangle2.setX(350.0f);
+        rectangle2.setY(200.0f);
+        rectangle2.setWidth(15.0f);
+        rectangle2.setHeight(200.0f);
+        rectangle2.setFill(Color.YELLOW);
+
+        rectangle3.setX(150.0f);
+        rectangle3.setY(200.0f);
+        rectangle3.setWidth(200.0f);
+        rectangle3.setHeight(15.0f);
+        rectangle3.setFill(Color.DEEPPINK);
+
+        rectangle4.setX(165.0f);
+        rectangle4.setY(385.0f);
+        rectangle4.setWidth(200.0f);
+        rectangle4.setHeight(15.0f);
+        rectangle4.setFill(Color.BLUE);
+
+        Image image = new Image("star.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(55);
+        imageView.setFitWidth(55);
+        imageView.setX(230);
+        imageView.setY(270);
+        imageView.setPreserveRatio(true);
+
+        Group root = new Group(rectangle,rectangle2,rectangle3,rectangle4,imageView);
+        RotateTransition rotateTransition = new RotateTransition();
+        rotateTransition.setNode(root);
+        rotateTransition.setDuration(Duration.millis(3000));
+        rotateTransition.setByAngle(360);
+        rotateTransition.setCycleCount(50);
+        rotateTransition.setAutoReverse(false);
+        rotateTransition.play();
+
+        return root;
+    }
+    public Group PlusObstacle() {
+
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle2 = new Rectangle();
+        Rectangle rectangle3 = new Rectangle();
+        Rectangle rectangle4 = new Rectangle();
+
+        rectangle.setX(180.0f);
+        rectangle.setY(200.0f);
+        rectangle.setWidth(15.0f);
+        rectangle.setHeight(70.0f);
+        rectangle.setFill(Color.AQUA);
+
+        rectangle2.setX(180.0f);
+        rectangle2.setY(270.0f);
+        rectangle2.setWidth(15.0f);
+        rectangle2.setHeight(70.0f);
+        rectangle2.setFill(Color.YELLOW);
+
+        rectangle3.setX(117.5f);
+        rectangle3.setY(262.5f);
+        rectangle3.setWidth(70.0f);
+        rectangle3.setHeight(15.0f);
+        rectangle3.setFill(Color.BLUE);
+
+        rectangle4.setX(187.5f);
+        rectangle4.setY(262.5f);
+        rectangle4.setWidth(70.0f);
+        rectangle4.setHeight(15.0f);
+        rectangle4.setFill(Color.DEEPPINK);
+
+
+        Group root = new Group(rectangle,rectangle2,rectangle3,rectangle4);
+        RotateTransition rotateTransition = new RotateTransition();
+        rotateTransition.setNode(root);
+        rotateTransition.setDuration(Duration.millis(3000));
+        rotateTransition.setByAngle(360);
+        rotateTransition.setCycleCount(50);
+        rotateTransition.setAutoReverse(false);
+        rotateTransition.play();
+
+        return root;
+    }
+
     public void GamePlay (Stage stage,Scene scene,Group root) {
 
         Text text = new Text();
@@ -451,13 +691,6 @@ public class MainGUI extends Application {
         text.setText("SCORE: 0  ");
         text.setFill(Color.WHITE);
 
-        Image image = new Image("star.png");
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(80);
-        imageView.setFitWidth(80);
-        imageView.setX(110);
-        imageView.setY(200);
-        imageView.setPreserveRatio(true);
 
         Button btn1 = new Button("Pause");
         btn1.setMinSize(100, 25);
@@ -479,7 +712,7 @@ public class MainGUI extends Application {
             System.out.println("exit");
             ExitGUI(stage,scene);
         });
-        root.getChildren().add(imageView);
+
         BorderPane border=new BorderPane();
         VBox ver=new VBox(btn1,btn2);
         ver.setSpacing(10);
